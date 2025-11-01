ccacadf498234afd78f4169f59c42df1963dd276
hellenlelemusi-source <hellenlelemusi@gmail.com>
2025-11-01 11:38:17 +0300

    Update 3D array occupancy calculation and output
    Fixed occupancy calculation and added random assignment for rooms.

--- 3D array.c
+++ 3D array.c
@@ -8,17 +8,33 @@ int main(){
 	int chain[3][5][10];
 	int branch,floor,room;
 	int totaloccupied=0;
+	//seed random number generator
+	srand(time(NULL));
 	//assign random accupancy
 	for(branch=0,branch<=3,branch++){
 	for (floor=0,floor<=5,floor++){
 	for (room=0,room<=10,room++){
-		if (chain[branch][floor][room]==1){
-		totaloccupied++;
+		chain[branch][floor][room]=rand()%2;
+		totaloccupied+=chain[branch][floor][room];
 	}
 	}	
 	}
+	//print occupancy matrix and calculate total occupied
+	for(branch=0,branch<=3,branch++){
+printf("branch%d:\n",branch+1);
+	for (floor=0,floor<=5,floor++){
+printf("floor%d:\n",floor+1);
+	for (room=0,room<=10,room++){
+		printf("%d",chain[branch][floor][room]);
 	}
+		printf("\n");
+	}
+		printf("\n");
+	}
+	//print total occupied rooms
+	
 	printf("total number of rooms across all branches:%d\n",totaloccupied);
 	return 0;
 }
+
 	
